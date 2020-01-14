d2de7c1bcc553dd6455e61f8234ae0927f5d67b4
Даниил Хозя <I.O.Stalina@hotmail.com>
2020-01-14 14:56:46 +0300

    Добавьте файлы проекта.

---
+++ STO/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using STO.Models;
+
+namespace STO.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private readonly IDetection _detection;
+        public HomeController(IDetection detection)
+        {
+            _detection = detection;
+        }
+        public IActionResult Index()
+        {
+            string browser_info = _detection.Browser.Type.ToString() + _detection.Browser.Version;
+            ViewData["a"] = browser_info;
+            return View(_detection);
+        }
+
+    }
+}
